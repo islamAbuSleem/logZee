97104a00fd264d0748398291476d7c8219d3564c
islamabusleem <islamabusleem@gmail.com>
2020-11-30 13:00:50 +0200

    responsive need some retouches

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,37 @@
-$('.dropdown-menu').addClass('invisible'); //FIRST TIME INVISIBLE
-
-// ADD SLIDEDOWN ANIMATION TO DROPDOWN-MENU 
-  $('.dropdown').on('show.bs.dropdown', function(e){
-    $('.dropdown-menu').removeClass('invisible');
-    $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+$(document).ready(function() {
+  $(".comment--slider").slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1
   });
+  $(".customer-say .comment--slider .slick-arrow").text("");
+  $(".customer-say .comment--slider .slick-prev").addClass(
+    "fa fa-chevron-left"
+  );
+  $(".customer-say .comment--slider .slick-next").addClass(
+    "fa fa-chevron-right"
+  );
+});
 
-  // ADD SLIDEUP ANIMATION TO DROPDOWN-MENU 
-  $('.dropdown').on('hide.bs.dropdown', function(e){
-    $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
-  });
-  
+$(".dropdown-menu").addClass("invisible"); //FIRST TIME INVISIBLE
+
+// ADD SLIDEDOWN ANIMATION TO DROPDOWN-MENU
+$(".dropdown").on("show.bs.dropdown", function(e) {
+  $(".dropdown-menu").removeClass("invisible");
+  $(this)
+    .find(".dropdown-menu")
+    .first()
+    .stop(true, true)
+    .slideDown();
+});
+
+// ADD SLIDEUP ANIMATION TO DROPDOWN-MENU
+$(".dropdown").on("hide.bs.dropdown", function(e) {
+  $(this)
+    .find(".dropdown-menu")
+    .first()
+    .stop(true, true)
+    .slideUp();
+});
+
+// comment slider
